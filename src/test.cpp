11b0cba7f900a82f9b770efc11b7330ea00e7487
ShelbyMitsuko <swong070@ucr.edu>
2023-06-06 23:58:34 -0700

    Add unit tests for Genre.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -1,12 +1,35 @@
 #include "gtest/gtest.h"
-#include "../header/Movielist.h"
+#include "../header/Director.h"
+#include "../header/Genre.h"
+#include "../header/MovieList.h"
 
 /*---------------------------Tests for Director.cpp---------------------------*/
 
-TEST (DirectorTest, SetDirector) {
+TEST(DirectorTest, SetDirector) {
     string testName = "Shelby Mitsuko";
     director.setDirector(testName);
     EXPECT_EQ(director.getDirector(), testName);
+}
+
+/*---------------------------------------------------------------------------*/
+
+/*----------------------------Tests for Genre.cpp----------------------------*/
+
+TEST(GenreTest, SetGenres) {
+    Genre genre;
+    vector<string> testGenres = {"Horror", "Comedy", "Romance"};
+    genre.setGenres(testGenres);
+    EXPECT_EQ(genre.getGenres(), testGenres);
+}
+
+TEST(GenreTest, GetGenres) {
+    Genre genre;
+    vector<string> testGenres = {"Horror", "Comedy", "Romance"};
+    genre.setGenres(testGenres);
+
+    vector<string> actualGenres = genre.getGenres();
+
+    EXPECT_EQ(actualGenres, testGenres);
 }
 
 /*---------------------------------------------------------------------------*/
