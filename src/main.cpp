ad2589087cd789e56ab380d8a76da075b62dcd97
jidud <jlee1392@ucr.edu>
2023-06-09 08:29:20 -0700

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -24,12 +24,12 @@ int main() {
 	int inpYear = 0;
 	double inpRating = 0.0;
 	string ans = "";
+	vector<string> genres {};
 
-	//g++ main.cpp Display.cpp Director.cpp Genre.cpp Movie.cpp MovieList.cpp Rating.cpp Title.cpp Year.cpp
-	//
+	//g++ src/main.cpp src/Display.cpp src/Director.cpp src/Genre.cpp src/Movie.cpp src/MovieList.cpp src/Rating.cpp src/Title.cpp src/Year.cpp 
 	MovieList list1;
-	list1.parseData("movieData.csv");
-	//
+	list1.parseData("moiveData.csv");
+
 
 	menu1.displayMainMenu();
 	cout << endl;
@@ -41,52 +41,62 @@ int main() {
 			cout << "Enter a keyword to filter: " << endl;
 			getline(cin, inpTitle);
 			list1.sortByTitle(inpTitle);
+			Movie movie(inpTitle, genres, inpDirector, inpYear, inpRating);
+			list1.addMovie(movie);
 			cout << endl;
 			cout << "-------------------------------------------------------------" << endl;
 			list1.printFive();
 			cout << "-------------------------------------------------------------" << endl;
 			cout << endl;
-			userOption = 6;
+			userOption = "6";
 		}
 		else if (userOption == "2") {
 			cout << "Enter a genre to sort by: " << endl;
 			getline(cin, inpGenre);
 			list1.sortByGenre(inpGenre);
+			Movie movie(inpTitle, genres, inpDirector, inpYear, inpRating);
+			list1.addMovie(movie);
 			cout << endl;
 			cout << "-------------------------------------------------------------" << endl;
 			list1.printFive();
 			cout << "-------------------------------------------------------------" << endl;
-			userOption = 6;
+			userOption = "6";
 		}
 		else if (userOption == "3") {
 			cout << "Enter a director to filter by: " << endl;
 			getline(cin, inpDirector);
 			list1.sortByDirector(inpDirector);
+			Movie movie(inpTitle, genres, inpDirector, inpYear, inpRating);
+			list1.addMovie(movie);
 			cout << endl;
 			cout << "-------------------------------------------------------------" << endl;
 			list1.printFive();
 			cout << "-------------------------------------------------------------" << endl;
-			userOption = 6;
+			userOption = "6";
 		}
 		else if (userOption == "4") {
 			cout << "Enter a year to filer by: " << endl;
 			cin >> inpYear;
 			list1.sortByReleaseYear(inpYear);
+			Movie movie(inpTitle, genres, inpDirector, inpYear, inpRating);
+			list1.addMovie(movie);
 			cout << endl;
 			cout << "-------------------------------------------------------------" << endl;
 			list1.printFive();
 			cout << "-------------------------------------------------------------" << endl;
-			userOption = 6;
+			userOption = "6";
 		}
 		else if (userOption == "5") {
 			cout << "Enter a rating to filer by: " << endl;
 			cin >> inpRating;
 			list1.sortByRating(inpRating);
+			Movie movie(inpTitle, genres, inpDirector, inpYear, inpRating);
+			list1.addMovie(movie);
 			cout << endl;
 			cout << "-------------------------------------------------------------" << endl;
 			list1.printFive();
 			cout << "-------------------------------------------------------------" << endl;
-			userOption = 6;
+			userOption = "6";
 		}
 		else if (userOption == "6") {
 			return 0;
